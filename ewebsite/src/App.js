20b4213d329ff698c851a14239fe8827c3b45d78
VijaayKumaar <118357550+VijaayKumaar@users.noreply.github.com>
2023-05-16 16:01:41 +0530

    Update App.js

--- ewebsite/src/App.js
+++ ewebsite/src/App.js
@@ -1,65 +1,10 @@
 
-// import './App.css';
-// // import "./components/header/Header"
-// import Header from './components/header/Header';
-// import Home from './components/Home/Home';
-// import { Box } from '@mui/material';
-
-//  import DataProvider from './context/Dataprovider';
-// import { BrowserRouter , Routes, Route } from 'react-router-dom';
-// import ItemView from './components/Home/items/ItemView';
 
 
-// function App() {
-//   return (
-//     <DataProvider>
-//       <BrowserRouter>
-//      <Header />
-//      <Box Style={{marginTop:54}} >
-//       <Routes>
-//      < Route path='/' element={<Home/>}/>
-//      {/* < Route path='/product/:Id'ItemView={<ItemView/>}/> */}
-//      <Route path= '/product/:id' element={<ItemView />} />
-//      </Routes>
-//      </Box>
-//      </BrowserRouter>
-//     </DataProvider>
-//   );
-// }
 
-// export default App;
 
-// import './App.css';
-// import Header from './components/header/Header';
-// import Home from './components/Home/Home';
-// import { Box } from '@mui/material';
-// import DataProvider from './context/Dataprovider';
-// import ProductProvider from './context/ProductProvider';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import ProductList from './components/Test/ProductProvider';
 
-// import DetailsItem from './components/Home/items/DetailsItem';
-// function App() {
-//   return (
-//     <DataProvider>
-     
-//       <BrowserRouter>
-//         <Header />
-//         <Box >
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-           
-//       {/* <Route path='product/:id' element={<DetailsItem/>} /> */}
-//           </Routes>
-          
-//         </Box>
-//       </BrowserRouter>
- 
-//     </DataProvider>
-//   );
-// }
 
-// export default App;
 
 
 
